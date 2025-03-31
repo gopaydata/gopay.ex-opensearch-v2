40727460a1db054408af867463a7e32b988fc144
gopay-keboola <109345035+gopay-keboola@users.noreply.github.com>
2025-03-31 15:40:42 +0200

    Update

--- src/component.py
+++ src/component.py
@@ -65,6 +65,7 @@ class Component(ComponentBase):
 
     def __init__(self):
         super().__init__()
+        self.ssh_tunnel = None
 
     def build_query(self, params):
         if "query" in params:
@@ -140,102 +141,102 @@ class Component(ComponentBase):
         self.validate_configuration_parameters(REQUIRED_PARAMETERS)
         params = self.configuration.parameters
 
-        if params.get(KEY_LEGACY_SSH):
-            self.run_legacy_client()
+        out_table_name = params.get(KEY_STORAGE_TABLE, "ex-elasticsearch-result")
+        logging.info(f"Using output table name: {out_table_name}")
+
+        user_defined_pk = params.get(KEY_PRIMARY_KEYS, [])
+        incremental = params.get(KEY_INCREMENTAL, False)
+
+        index_name = params.get(KEY_INDEX_NAME)
+        query = self.build_query(params)
+        logging.info(f"Extracting data from index: {index_name}")
+
+        range_query = query.get('query', {}).get('range', {})
+        if range_query:
+            for field, condition in range_query.items():
+                gte = condition.get('gte', 'not specified')
+                lte = condition.get('lte', 'not specified')
+                logging.info(f"Time range on field '{field}': from {gte} to {lte}")
+
+        logging.info(f"Full query: {json.dumps(query)}")
+
+        statefile = self.get_state_file()
+
+        ssh_options = params.get(KEY_SSH)
+        if ssh_options and ssh_options.get(KEY_USE_SSH, False):
+            self._create_and_start_ssh_tunnel(ssh_options, params)
+
+        if self.ssh_tunnel and self.ssh_tunnel.is_active:
+            logging.info(f"SSH Tunnel is active. Local bind address: {self.ssh_tunnel.local_bind_address}")
         else:
-            out_table_name = params.get(KEY_STORAGE_TABLE, "ex-elasticsearch-result")
-            logging.info(f"Using output table name: {out_table_name}")
+            logging.info("SSH Tunnel is not active or not configured.")
 
-            user_defined_pk = params.get(KEY_PRIMARY_KEYS, [])
-            incremental = params.get(KEY_INCREMENTAL, False)
+        client = self.get_client(params)
 
-            index_name = params.get(KEY_INDEX_NAME)
-            query = self.build_query(params)
-            logging.info(f"Extracting data from index: {index_name}")
+        temp_folder = os.path.join(self.data_folder_path, "temp")
+        os.makedirs(temp_folder, exist_ok=True)
 
-            range_query = query.get('query', {}).get('range', {})
-            if range_query:
-                for field, condition in range_query.items():
-                    gte = condition.get('gte', 'not specified')
-                    lte = condition.get('lte', 'not specified')
-                    logging.info(f"Time range on field '{field}': from {gte} to {lte}")
+        columns = statefile.get(out_table_name, [])
+        out_table = self.create_out_table_definition(out_table_name, primary_key=user_defined_pk, incremental=incremental)
 
-            logging.info(f"Full query: {json.dumps(query)}")
+        doc_count = 0
+        try:
+            with ElasticDictWriter(out_table.full_path, columns) as wr:
+                for result in client.extract_data(index_name, query):
+                    wr.writerow(result)
+                    doc_count += 1
+                    if doc_count % 10000 == 0:
+                        logging.info(f"Downloaded {doc_count} documents so far...")
+                wr.writeheader()
+        except Exception as e:
+            raise UserException(f"Error occured while extracting data from OpenSearch: {e}")
+        finally:
+            if self.ssh_tunnel and self.ssh_tunnel.is_active:
+                logging.info("Stopping SSH Tunnel.")
+                self.ssh_tunnel.stop()
 
-            statefile = self.get_state_file()
+        logging.info(f"Total downloaded documents: {doc_count}")
 
-            ssh_options = params.get(KEY_SSH)
-            if not isinstance(ssh_options, list) and ssh_options.get(KEY_USE_SSH, False):
-                self._create_and_start_ssh_tunnel(params)
+        self.write_manifest(out_table)
+        statefile[out_table_name] = wr.fieldnames
+        self.write_state_file(statefile)
+        self.cleanup(temp_folder)
 
-            if hasattr(self, "ssh_tunnel") and self.ssh_tunnel.is_active:
-                logging.info(f"SSH Tunnel is active. Local bind address: {self.ssh_tunnel.local_bind_address}")
-            else:
-                logging.info("SSH Tunnel is not active or not configured.")
+    @staticmethod
+    def is_valid_rsa(rsa_key):
+        """Validates the RSA private key."""
+        if not rsa_key.startswith(RSA_HEADER):
+            return False, "RSA key does not start with the correct header."
+        if "\n" not in rsa_key:
+            return False, "RSA key does not contain newline characters."
+        return True, ""
 
-            client = self.get_client(params)
+    def _create_and_start_ssh_tunnel(self, ssh_options, params):
+        ssh_username = ssh_options.get(KEY_SSH_USERNAME)
+        private_key = ssh_options.get(KEY_SSH_KEYS, {}).get(KEY_SSH_PRIVATE_KEY)
+        ssh_tunnel_host = ssh_options.get(KEY_SSH_TUNNEL_HOST)
+        ssh_tunnel_port = ssh_options.get(KEY_SSH_TUNNEL_PORT, 22)
 
-            temp_folder = os.path.join(self.data_folder_path, "temp")
-            os.makedirs(temp_folder, exist_ok=True)
-
-            columns = statefile.get(out_table_name, [])
-            out_table = self.create_out_table_definition(out_table_name, primary_key=user_defined_pk,
-                                                         incremental=incremental)
-
-            doc_count = 0
-            try:
-                with ElasticDictWriter(out_table.full_path, columns) as wr:
-                    for result in client.extract_data(index_name, query):
-                        wr.writerow(result)
-                        doc_count += 1
-                        if doc_count % 10000 == 0:
-                            logging.info(f"Downloaded {doc_count} documents so far...")
-                    wr.writeheader()
-            except Exception as e:
-                raise UserException(f"Error occured while extracting data from OpenSearch: {e}")
-            finally:
-                if hasattr(self, 'ssh_tunnel') and self.ssh_tunnel.is_active:
-                    logging.info("Stopping SSH Tunnel.")
-                    self.ssh_tunnel.stop()
-
-            logging.info(f"Total downloaded documents: {doc_count}")
-
-            self.write_manifest(out_table)
-            statefile[out_table_name] = wr.fieldnames
-            self.write_state_file(statefile)
-            self.cleanup(temp_folder)
-
-    def _create_and_start_ssh_tunnel(self, params) -> None:
-        ssh_params = params.get(KEY_SSH)
-        ssh_username = ssh_params.get(KEY_SSH_USERNAME)
-        keys = ssh_params.get(KEY_SSH_KEYS)
-        private_key = keys.get(KEY_SSH_PRIVATE_KEY)
-        ssh_tunnel_host = ssh_params.get(KEY_SSH_TUNNEL_HOST)
-        ssh_tunnel_port = ssh_params.get(KEY_SSH_TUNNEL_PORT, 22)
         db_params = params.get(KEY_GROUP_DB)
         db_hostname = db_params.get(KEY_DB_HOSTNAME)
-        db_port = db_params.get(KEY_DB_PORT)
+        db_port = int(db_params.get(KEY_DB_PORT))
 
-        is_valid, error_message = self.is_valid_rsa(private_key)
-        if not is_valid:
-            raise UserException(f"Invalid RSA key provided: {error_message}")
+        if not private_key.startswith(RSA_HEADER) or "\n" not in private_key:
+            raise UserException("Invalid RSA private key.")
 
         try:
             private_key = get_private_key(private_key, None)
         except SomeSSHException as e:
             raise UserException(e) from e
 
-        try:
-            db_port = int(db_port)
-        except ValueError as e:
-            raise UserException("Remote port must be a valid integer") from e
-
         self.ssh_tunnel = SSHTunnelForwarder(
             ssh_address_or_host=(ssh_tunnel_host, ssh_tunnel_port),
             ssh_username=ssh_username,
             ssh_pkey=private_key,
             remote_bind_address=(db_hostname, db_port),
-            local_bind_address=(LOCAL_BIND_ADDRESS, 0)
+            local_bind_address=(LOCAL_BIND_ADDRESS, 0),  # auto-assign local port
+            ssh_config_file=None,
+            allow_agent=False
         )
 
         try:
@@ -243,7 +244,7 @@ class Component(ComponentBase):
         except BaseSSHTunnelForwarderError as e:
             raise UserException("Failed to establish SSH connection. Recheck all SSH configuration parameters") from e
 
-        logging.info(f"SSH Tunnel is enabled: {self.ssh_tunnel.local_bind_address}")
+        logging.info("SSH tunnel is enabled and started.")
 
     @staticmethod
     def run_legacy_client():
